7f628ac7a314d00c4348bd5646ad4eb38bdd2faa
ItsManavKumar <manav.kumar@student.uts.edu.au>
2024-08-02 00:14:52 +1000

    removed [id].tsx that was causing build failure

--- src/components/newTweetForm.tsx
+++ src/components/newTweetForm.tsx
@@ -1,8 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/prefer-optional-chain */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { useSession } from "next-auth/react";
 import {
   FormEvent,
